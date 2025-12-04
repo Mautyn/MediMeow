d307305c15ea26a49ba4b42c69d4f132344e31a0
Mautyn <piotr.martyn@wp.pl>
2025-12-04 11:38:24 +0100

    -

--- feature_home/src/main/java/pl/edu/pk/student/feature_home/navigation/HomeDestinations.kt
+++
@@ -1,7 +0,0 @@
-package pl.edu.pk.student.feature_home.navigation
-
-sealed class HomeDestinations(val route: String) {
-    object Dashboard : HomeDestinations("dashboard")
-    object Profile : HomeDestinations("profile")
-    object Settings : HomeDestinations("settings")
-}
